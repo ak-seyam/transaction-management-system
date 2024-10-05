e05749d9e041032281d285e8e5586a3dfeded66a
ak-seyam <abdullah.khaled.kamal0@gmail.com>
2024-10-05 23:37:56 +0300

    Feat: add authorization notification

--- src/services/analytics.service.ts
+++ src/services/analytics.service.ts
@@ -1,10 +1,43 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { Events } from 'src/common/constants';
 import { TransactionEvent } from 'src/common/proto/service';
 
 @Injectable()
 export class AnalyticsService {
-  async sendTransactionEvent(event: TransactionEvent): Promise<boolean> {
-    Logger.debug(`sending event ${event} to analytics`);
+  @OnEvent(Events.TRANSACTION_CLEARING_ACCEPTED)
+  async sendClearingAcceptedNotification(
+    event: TransactionEvent,
+  ): Promise<boolean> {
+    // Assume that it will always work
+    Logger.debug(`sending clearing accepted analytics: event ${event}`);
+    return true;
+  }
+
+  @OnEvent(Events.TRANSACTION_CLEARING_REJECTED)
+  async sendClearingRejectedNotification(
+    event: TransactionEvent,
+  ): Promise<boolean> {
+    // Assume that it will always work
+    Logger.debug(`sending clearing rejected analytics: event ${event}`);
+    return true;
+  }
+
+  @OnEvent(Events.TRANSACTION_AUTHORIZING_REJECTED)
+  async sendAuthorizingRejectedNotification(
+    event: TransactionEvent,
+  ): Promise<boolean> {
+    // Assume that it will always work
+    Logger.debug(`sending authorizing rejected analytics: event ${event}`);
+    return true;
+  }
+
+  @OnEvent(Events.TRANSACTION_AUTHORIZING_ACCEPTED)
+  async sendAuthorizingAcceptedNotification(
+    event: TransactionEvent,
+  ): Promise<boolean> {
+    // Assume that it will always work
+    Logger.debug(`sending authorizing accepted analytics: event ${event}`);
     return true;
   }
 }
--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,14 +1,43 @@
 import { Injectable, Logger } from '@nestjs/common';
-import UserDetails from 'src/user/user-details';
+import { OnEvent } from '@nestjs/event-emitter';
+import { Events } from '../common/constants';
+import { TransactionEvent } from 'src/common/proto/service';
 
 @Injectable()
 export class NotificationService {
-  async sendNotification(
-    userDetails: UserDetails,
-    message: string,
+  @OnEvent(Events.TRANSACTION_CLEARING_ACCEPTED)
+  async sendClearingAcceptedNotification(
+    event: TransactionEvent,
   ): Promise<boolean> {
     // Assume that it will always work
-    Logger.debug(`sending message ${message} to ${userDetails.phoneNumber}`);
+    Logger.debug(`sending clearing accepted notification: event ${event}`);
+    return true;
+  }
+
+  @OnEvent(Events.TRANSACTION_CLEARING_REJECTED)
+  async sendClearingRejectedNotification(
+    event: TransactionEvent,
+  ): Promise<boolean> {
+    // Assume that it will always work
+    Logger.debug(`sending clearing rejected notification: event ${event}`);
+    return true;
+  }
+
+  @OnEvent(Events.TRANSACTION_AUTHORIZING_REJECTED)
+  async sendAuthorizingRejectedNotification(
+    event: TransactionEvent,
+  ): Promise<boolean> {
+    // Assume that it will always work
+    Logger.debug(`sending authorizing rejected notification: event ${event}`);
+    return true;
+  }
+
+  @OnEvent(Events.TRANSACTION_AUTHORIZING_ACCEPTED)
+  async sendAuthorizingAcceptedNotification(
+    event: TransactionEvent,
+  ): Promise<boolean> {
+    // Assume that it will always work
+    Logger.debug(`sending authorizing accepted notification: event ${event}`);
     return true;
   }
 }
